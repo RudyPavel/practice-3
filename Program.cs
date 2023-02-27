05c6cfbad43c4644bdd8cdd39bf85e102a0e7e4c
RudyPavel <rudyy.pavlik@mail.ru>
2023-02-27 23:27:24 +0200

    Practice 19

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int ostatok, sum = 0;
+int temp = number;
+
+while (number > 0)
+{
+    ostatok = number % 10;
+    
+    sum = (sum * 10) + ostatok;
+    
+    number = number / 10;
+}
+
+if (temp == sum)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
