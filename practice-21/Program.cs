ac7c782d187918b2d6cf3294f5848a734ae34632
RudyPavel <rudyy.pavlik@mail.ru>
2023-02-28 00:00:33 +0200

    practice 3

---
+++ practice-21/Program.cs
@@ -0,0 +1,15 @@
+﻿string line1 = Console.ReadLine();
+string line2 = Console.ReadLine();
+
+string[] line1Arr = line1.Split(' ');
+int[] vactor1 = Array.ConvertAll(line1Arr, int.Parse);
+string[] line2Arr = line2.Split(' ');
+int[] vactor2 = Array.ConvertAll(line2Arr, int.Parse);
+
+float deltaX = vactor1[0] - vactor2[0];
+float deltaY = vactor1[1] - vactor2[1];
+float deltaZ = vactor1[2] - vactor2[2];
+
+float distance = (float) Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+
+Console.WriteLine(distance.ToString("0.00"));
